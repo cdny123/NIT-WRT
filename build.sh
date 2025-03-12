5e1e1ff47a46f0fb60f6a3334b0ca178ce4542ca
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-03-12 15:07:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
-# 一键编译脚本
+# 本地编译
+chmod +x build.sh
+./build.sh
 
-#!/bin/bash
-
-# 初始化配置
-chmod +x diy/scripts/*.sh
-./diy/scripts/base_config.sh
-git apply diy/patches/*.patch
-
-# 合并配置
-cat diy/x86-64.config >> .config
-make defconfig
-
-# 编译流程
-make -j$(nproc) download
-make -j$(($(nproc)+1)) V=s
+# 或通过GitHub Actions：
+git add .
+git commit -m "更新配置"
+git push
