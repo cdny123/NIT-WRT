1db93e86849522c35bcf6c2a165bd2b61242b496
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-03-15 08:45:59 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# 添加软件源
-echo "src-git custom_pkg https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
+# 添加软件源到 feeds.conf.default 文件
+# echo "src-git custom_pkg https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
+echo "src-git cdny123 https://github.com/cdny123/openwrt-package1.git" >> openwrt/feeds.conf.default
 
 # 添加自定义软件
 git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
 git clone -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp       # 一键自动格式化分区、扩容、自动挂载插件
 
 # 添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
