42ea92c3cc08eb06602e65f957b462c9cb2c54e6
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-03-16 21:11:25 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,13 +5,15 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo "src-git custom_pkg https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
 # echo "src-git cdny123 https://github.com/cdny123/openwrt-package1.git" >> openwrt/feeds.conf.default
 
+# 在 DIY 脚本中调整依赖
+sed -i 's/dnsmasq/dnsmasq-full/g' package/feeds/luci/luci-app-dhcp/Makefile
+
 # 添加自定义软件
 git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
 git clone -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp       # 一键自动格式化分区、扩容、自动挂载插件
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-
 
 # 添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
