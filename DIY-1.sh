67bbc2a12caf5f29d41d166ae1d67421adc8df05
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-03-18 13:26:26 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,6 +2,7 @@
 # 添加软件源到 feeds.conf.default 文件
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 # echo "src-git custom_pkg https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
 # echo "src-git cdny123 https://github.com/cdny123/openwrt-package1.git" >> openwrt/feeds.conf.default
