5fe2c2c12e6e92c1cbeee5f418b05b0025ad19f9
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-03-18 10:44:11 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,6 +2,7 @@
 # 添加软件源到 feeds.conf.default 文件
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i 'src-git infinityfreedomng https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 # echo "src-git custom_pkg https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
 # echo "src-git cdny123 https://github.com/cdny123/openwrt-package1.git" >> openwrt/feeds.conf.default
 
@@ -14,6 +15,7 @@ git clone https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
 git clone -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp       # 一键自动格式化分区、扩容、自动挂载插件
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # 添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
